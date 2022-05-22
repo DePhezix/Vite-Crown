2cc00e34e9ddbcc8bdb0927a2d3206fdc3a7c38f
Mirzokhid Nazirov <mirzokhidnazirov97@gmail.com>
2022-05-22 08:17:40 +0500

    transfered files

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import Navigation from "./routes/navigation/navigation.component";
+import SignUpForm from "./components/sign-up-form/sign-up-form.component";
+import SignInForm from "./components/sign-in-form/sign-in-form.component";
+import { useContext } from "react";
+import { UserContext } from "./contexts/user.context";
+import {Navigate, Routes, Route} from 'react-router-dom';
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
+const App = () => {
+  const { currentUser } = useContext(UserContext);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route
+          path="home"
+          element={currentUser ? <Home /> : <Navigate to="/" replace />}
+        />
+        <Route
+          path="shop"
+          element={currentUser ? <Shop /> : <Navigate to="/" replace />}
+        />
+        <Route
+          index
+          element={
+            currentUser ? <Navigate to="/home" replace /> : <SignInForm />
+          }
+        />
+        <Route
+          path="/signUp"
+          element={
+            currentUser ? <Navigate to="/home" replace /> : <SignUpForm />
+          }
+        />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,79 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHQwP3GrxAfjzZ1cuPcz04wLJCnxyfCsY",
+  authDomain: "crown-18057.firebaseapp.com",
+  projectId: "crown-18057",
+  storageBucket: "crown-18057.appspot.com",
+  messagingSenderId: "348693292316",
+  appId: "1:348693292316:web:3129d19fe669e6da2246d9",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
